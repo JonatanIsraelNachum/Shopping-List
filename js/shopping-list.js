1fc0b3e582caabd55d7047559c2becb41061d18e
JonatanIsraelNachum <gonatan1996@gmail.com>
2025-03-03 21:32:45 +0200

    push

--- js/shopping-list.js
+++ js/shopping-list.js
@@ -25,72 +25,79 @@ const syncWithNetlify = async () => {
     }
 };
 
-const loadFromNetlify = async () => {
-    try {
-        const res = await fetch("https://listofshopping.netlify.app/.netlify/functions/updateList");
-        const data = await res.json();
-
-        if (data && data.list && data.list.length > 0) {
-            prod_arr = JSON.parse(data.list).map(item => new Product(item.category.name, item.name, item.amount));
-            updateLocalStorage();
-        } else {
-            console.warn("Server returned empty list, loading from localStorage.");
-            loadFromLocalStorage();
-        }
-    } catch (error) {
-        console.error("Fetch error:", error.message, "Loading from localStorage instead.");
-        loadFromLocalStorage();
-    }
-    renderAllProducts();
-};
 // const loadFromNetlify = async () => {
 //     try {
-//         loadProducts()
-//         // try {
-//         //     await fetch("https://shoppingli.netlify.app/.netlify/functions/updateList", {
-//         //         method: "POST",
-//         //         headers: { "Content-Type": "application/json" },
-//         //         body: JSON.stringify({ list: [] }), // שולח רשימה ריקה לשרת
-//         //     });
-//         //     console.log("Netlify data cleared.");
-//         // } catch (err) {
-//         //     console.error("Clear error:", err);
-//         // }
-//         try {
-//             const res = await fetch("https://listofshopping.netlify.app/.netlify/functions/updateList");
-//             const data = await res.json();
-            
-//             if (data && data.list) {
-//                 const serverData = data.list;
-//                 const localData = prod_arr;
-//                 const mergedData = [...serverData];
-//                 serverData.forEach((item)=>{
-//                     console.log(item);
-//                     console.log(item.name);
-//                 })
-//                 console.log("merge");
-                
-//                 mergedData.forEach((item)=>{
-//                     console.log(item);
-//                     console.log(item.name);
-//                 })
+//         const res = await fetch("https://listofshopping.netlify.app/.netlify/functions/updateList");
+//         const data = await res.json();
 
-//                 localData.forEach(localItem => {
-//                     if (!mergedData.some(serverItem => localItem.name === serverItem.name)) {
-//                         mergedData.push(localItem);
-//                     }
-//                 });
-//                 prod_arr = mergedData;
-//                 updateLocalStorage();
-//                 loadProducts();
-//             }
-//         } catch (error) {
-//             console.error("Fetch error:", error.message);
+//         if (data && data.list && data.list.length > 0) {
+//             serverData.forEach((item)=>{
+//                                     console.log(item);
+//                                     console.log(item.name);
+//                                 })
+
+
+//             console.log(data.length);
+//             prod_arr = JSON.parse(data.list).map(item => new Product(item.category.name, item.name, item.amount));
+//             updateLocalStorage();
+//         } else {
+//             console.warn("Server returned empty list, loading from localStorage.");
+//             loadFromLocalStorage();
 //         }
 //     } catch (error) {
-//         console.error("Failed to load data:", error);
+//         console.error("Fetch error:", error.message, "Loading from localStorage instead.");
+//         loadFromLocalStorage();
 //     }
+//     renderAllProducts();
 // };
+const loadFromNetlify = async () => {
+    try {
+        loadProducts()
+        // try {
+        //     await fetch("https://shoppingli.netlify.app/.netlify/functions/updateList", {
+        //         method: "POST",
+        //         headers: { "Content-Type": "application/json" },
+        //         body: JSON.stringify({ list: [] }), // שולח רשימה ריקה לשרת
+        //     });
+        //     console.log("Netlify data cleared.");
+        // } catch (err) {
+        //     console.error("Clear error:", err);
+        // }
+        try {
+            const res = await fetch("https://listofshopping.netlify.app/.netlify/functions/updateList");
+            const data = await res.json();
+            
+            if (data && data.list && data.list.length > 0) {
+                const serverData = data.list;
+                const localData = prod_arr;
+                const mergedData = [...serverData];
+                serverData.forEach((item)=>{
+                    console.log(item);
+                    console.log(item.name);
+                })
+                console.log("merge");
+                
+                mergedData.forEach((item)=>{
+                    console.log(item);
+                    console.log(item.name);
+                })
+
+                localData.forEach(localItem => {
+                    if (!mergedData.some(serverItem => localItem.name === serverItem.name)) {
+                        mergedData.push(localItem);
+                    }
+                });
+                prod_arr = mergedData;
+                updateLocalStorage();
+                loadProducts();
+            }
+        } catch (error) {
+            console.error("Fetch error:", error.message);
+        }
+    } catch (error) {
+        console.error("Failed to load data:", error);
+    }
+};
 const loadFromLocalStorage = () => {
     const list = localStorage.getItem('shoppingList');
     if (list) {
